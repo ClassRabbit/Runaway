2db4ad8e82575c7c8b2ca9ebb8e3832f2b1bc9bb
Minchan Kim <livingkmc@gmail.com>
2017-02-05 17:44:32 +0900

    구름 추가

---
+++ Assets/Editor/SkyEditor.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(Sky))]
+public class SkyEditor : Editor {
+
+	Sky sky;
+	string cloudName = "Cloud ({0})";
+
+	void Awake()
+	{
+		sky = target as Sky;
+
+	}
+
+	public override void OnInspectorGUI()
+	{
+		base.OnInspectorGUI();
+
+
+		if (GUILayout.Button("AddCloud"))
+		{
+			GameObject newCloud = (GameObject)GameObject.Instantiate(sky.cloudPrefab);
+
+			int cloudCount = sky.transform.childCount;
+
+			newCloud.transform.parent = sky.transform;
+
+			newCloud.name = string.Format(cloudName, cloudCount+1);
+
+			newCloud.transform.position = new Vector3(sky.transform.position.x, sky.transform.position.y, 
+			                                          sky.transform.position.z);
+
+		}
+	}
+}
---
+++ Assets/Scripts/Map/Cloud.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cloud : MonoBehaviour {
+
+	public float speed = 1.0f;
+	GameObject sky;
+
+	float startPosition;
+	float endPosition;
+
+	void Start()
+	{
+		sky = transform.parent.gameObject;
+
+		Transform skyTransform = sky.transform;
+		float skyPositionX = skyTransform.position.x;
+		float range = skyTransform.localScale.x;
+
+		startPosition = skyPositionX + (range / 2);
+		endPosition = skyPositionX - (range / 2);
+	}
+
+	void Update()
+	{
+		transform.Translate(-1 * speed * Time.deltaTime, 0, 0);
+		if (transform.position.x <= endPosition)
+		{
+			ScrollEnd();
+		}
+	}
+
+	void ScrollEnd()
+	{
+		transform.Translate(-1 * (endPosition - startPosition), 0, 0);
+
+		//SendMessage("OnScrollEnd", SendMessageOptions.DontRequireReceiver);
+	}
+
+}
